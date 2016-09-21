4a7cfcd445243d7b56af01b30dead7c4cb454bc8
Matthew Budde <matthew.budde.mb@gmail.com>
2016-09-21 05:51:14 +0000

    Added a third scan to each processing Added a distance qualifier Added a failed beacon finding count

--- triangulation.py
+++ triangulation.py
@@ -1,4 +1,3 @@
-import rospy
 import roslib
 from std_msgs.msg import String
 import sensor_msgs.msg
@@ -6,7 +5,7 @@ from math import *
 
 # Threshold for determining which data points correspond to beacons
 global THRESHOLD
-THRESHOLD = 18
+THRESHOLD = 19
 
 # These will later be initialized as 2D arrays
 global scanData
@@ -26,6 +25,9 @@ scanCount = 0
 global numberScans
 numberScans = 0
 
+global missedScans
+missedScans = 0
+
 # Callback function for a LiDAR scan
 def callback(data):
 	# Pull all data from the sensor message into local variables
@@ -41,15 +43,18 @@ def callback(data):
 	# If there is only one scan available, store in memory
 	if( scanCount == 0 ):
 		firstScan( startAngle, endAngle, angleInc, ranges, intensities )
-	# If two scans have been obtained, combine them and attempt to find the position of the device
 	if( scanCount == 1 ):
+		secondScan( startAngle, endAngle, angleInc, ranges, intensities, scanData1 )
+	# If two scans have been obtained, combine them and attempt to find the position of the device
+	if( scanCount == 2 ):
 		processScan( startAngle, endAngle, angleInc, ranges, intensities, scanData1 )
 		findBeacons( scanData, angleInc )
 		calcPosition( beaconX, beaconY, beaconData )
 		calcAngle( )
 		numberScans = numberScans + 1
 		print( numberScans )
-	scanCount = ( scanCount + 1 ) % 2 # Roll the scan count around
+		print( missedScans )
+	scanCount = ( scanCount + 1 ) % 3 # Roll the scan count around
 
 # Function to process the first scan - using two scans to get a suitable number of high-quality data points
 def firstScan( start, end, inc, ranges, intensities ):
@@ -63,6 +68,28 @@ def firstScan( start, end, inc, ranges, intensities ):
 		if intensities[i] > THRESHOLD:
 			scanData1[0].append( start + ( i * inc ) )
 			scanData1[1].append( ranges[i] )
+
+# Function to process the second scan - using three scans
+def secondScan( start, end, inc, ranges, intensities, scanData1 ):
+	scanTemp = []
+	scanTemp.append( [] )
+	scanTemp.append( [] )
+	# Temporarily store the scan data
+	for i in range( len( intensities ) ):
+		if intensities[i] > THRESHOLD:
+			scanTemp[0].append( start + ( i * inc ) )
+			scanTemp[1].append( ranges[i] )
+	
+	# Insert the temporary scan into the global array
+	k = 0
+	l = 0
+	while l < len( scanData1[0] ) and k < len( scanTemp[0] ):
+		if( scanTemp[0][k] < scanData1[0][1] ):
+			scanData1[0].insert( 1, scanTemp[0][k] )
+			scanData1[1].insert( 1, scanTemp[1][k] )
+			k = k + 1
+		else:
+			l = l + 1
 
 # Function to process both scans together
 def processScan( start, end, inc, ranges, intensities, scanData1 ):
@@ -109,11 +136,9 @@ def findBeacons( scanData, inc ):
 
 	# Find the average difference in angle from point to point
 	avgDiffTheta = 0
-	avgDiffCount = 0
 	for i in range( 1, len( scanData[0] ) ):
 		avgDiffTheta = avgDiffTheta + ( scanData[0][i] - scanData[0][i-1] )
-		avgDiffCount = avgDiffCount + 1
-	avgDiffTheta = avgDiffTheta / avgDiffCount + inc / 2
+	avgDiffTheta = avgDiffTheta / ( len( scanData[0] ) - 1 )
 
 	# Loop through the length of the scan data
 	for i in range( len( scanData[0] ) - 1 ):
@@ -121,7 +146,7 @@ def findBeacons( scanData, inc ):
 		if( beaconsFound < 3 ):
 			# Check to see if the angle between two points is less than the average
 			# Indicates that these are two points on one beacon rather than on two beacons
-			if( abs( scanData[0][i] - scanData[0][i + 1] ) < avgDiffTheta ):
+			if( abs( scanData[0][i] - scanData[0][i + 1] ) < avgDiffTheta and scanData[1][i] < 2 ):
 				if( started == 0 ):
 					beaconStart = i
 					started = 1
@@ -196,6 +221,8 @@ def calcPosition( beaconX, beaconY, beaconData ):
 		options[1].append( beaconX[0] + rectangularTranslatedX[1] )
 		options[1].append( beaconY[0] + rectangularTranslatedY[1] )
 
+		print( options )
+
 		d1 = sqrt( pow( options[0][0] - beaconX[2], 2 ) + pow( options[0][1] - beaconY[2], 2 ) )
 		d2 = sqrt( pow( options[1][0] - beaconX[2], 2 ) + pow( options[1][1] - beaconY[2], 2 ) )
 		if( abs( d1 - beaconData[1][2] ) < abs( d2 - beaconData[1][2] ) ):
@@ -209,6 +236,8 @@ def calcPosition( beaconX, beaconY, beaconData ):
 	else:
 		print( "NOT ENOUGH BEACONS FOUND" )
 		print( "" )
+		global missedScans
+		missedScans = missedScans + 1
 
 def calcAngle():	
 	print( "ANGLE" )
